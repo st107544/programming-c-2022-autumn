bdd8a9b1f9bcf86da4a1e5d750d7e90a48c0fba6
st107544 <st107544@student.spbu.ru>
2023-02-27 10:58:03 +0300

    господи христа ради простите, что не в срок отправила вам выполненное задание((( Я очень сожалею, поставьте оценку

---
+++ 2023.02.15-Homework1/Task1/Source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+struct Animal
+{
+	std::string type;
+	std::string name;
+	int age;
+
+	Animal(std::string type = "pig", std::string name = "Peppa", int age = 3)
+	{
+		this->type = type;
+		this->name = name;
+		this->age = age;
+	}
+
+	Animal(const Animal& beast)
+	{
+		this->type = beast.type;
+		this->name = beast.name;
+		this->age = beast.age;
+	}
+
+	~Animal()
+	{
+	}
+};
+
+void printAnimal(Animal animal)
+{
+	std::cout << "Name: " << animal.name << '.' << std::endl;
+	std::cout << "Type: " << animal.type << '.' << std::endl;
+	std::cout << "Age: " << animal.age << "." << std::endl;
+}
+
+void elderAnimal(Animal& animal)
+{
+	animal.age += 1;
+}
+
+int main(int argc, char* argv[])
+{
+	Animal Donkey("donkey", "Donkey", 28);
+	elderAnimal(Donkey);
+	printAnimal(Donkey);
+	return EXIT_SUCCESS;
+}
